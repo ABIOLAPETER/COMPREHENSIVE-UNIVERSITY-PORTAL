1ed09f35dbc22addd1e8e42457cccbe00a667646
peter abiola <peterboluwatife69@gmail.com>
2026-03-07 10:39:23 +0100

    fix: use shared redis client in rate limiter

--- api/src/shared/middleware/rate-limiter.midlleware.ts
+++ api/src/shared/middleware/rate-limiter.midlleware.ts
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
-import { redisClient } from "../shared/utils/redis"; // import your existing client
+import { redisClient } from "../utils/redis"; // import your existing client
 
 export const gatewayRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
